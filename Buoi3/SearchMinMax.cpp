ca00321384280dc555c7f8b05c7b1e1fc27d288f
Lelinh23 <lttlinh.20it9@vku.udn.vn>
2023-03-03 13:47:02 +0700

    first commit

---
+++ Buoi3/SearchMinMax.cpp
@@ -0,0 +1,27 @@
+#include <cstdlib> 
+#include <iostream> 
+using namespace std; 
+int main(int argc, char *argv[]) 
+{ 
+  int arr[5]; 
+  int mode[5][2]; 
+   cout<<"Nhap 10 so nguyen\n"; 
+  for(int l=0;l<5;l++) {
+     cout<<"Gia tri "<<l<<" la :";cin>>arr[l]; 
+}  
+ //tim gia tri lon nhat va nho nhat 
+ int i,j,temp; 
+//sap xep mang de tim gia tri lon nhat va nho nhat 
+ for(i=0;i<5;i++)
+  for(j=4;j>i;j--)
+     if(arr[j]<arr[j-1]) {
+         int temp=arr[j]; 
+         arr[j]=arr[j-1]; 
+         arr[j-1]=temp;
+}
+                  
+  cout<<"Gia tri lon nhat="<<arr[4]<<"\nGia tri nho nhat="<<arr[0]; 
+  cout<<"\n"; 
+  
+  return 0; 
+}
